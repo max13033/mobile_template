566bc0b210a4e1b9afd44251fffa6234c88ee81f
max13033 <max1303@rambler.ru>
2020-03-23 19:53:05 +0300

    add footer_0.php

--- components/bitrix/socserv.auth.form/icons/template.php
+++ components/bitrix/socserv.auth.form/icons/template.php
@@ -5,7 +5,11 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true)
 <div class="col-12 modal-fb">
     <div class="bx-auth-serv-icons">
         <?foreach($arParams["~AUTH_SERVICES"] as $service):?>
-            <a title="<?=htmlspecialcharsbx($service["NAME"])?>" href="javascript:void(0)" onclick="BxShowAuthFloat('<?=$service["ID"]?>', '<?=$arParams["SUFFIX"]?>')"><i class="bx-ss-icon <?=htmlspecialcharsbx($service["ICON"])?>"></i></a>
+            <a title="<?=htmlspecialcharsbx($service["NAME"])?>" 
+                href="javascript:void(0)" 
+                onclick="BxShowAuthFloat('<?=$service["ID"]?>', '<?=$arParams["SUFFIX"]?>')">
+                <i class="bx-ss-icon <?=htmlspecialcharsbx($service["ICON"])?>"></i>
+            </a>
         <?endforeach?>
     </div>
 </div>
