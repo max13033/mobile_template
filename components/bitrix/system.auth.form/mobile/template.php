84aeac6ccff2bb5041aca9ff01e8ec79c2c8c71b
max13033 <max1303@rambler.ru>
2020-03-25 19:13:21 +0300

    change header fix position

--- components/bitrix/system.auth.form/mobile/template.php
+++ components/bitrix/system.auth.form/mobile/template.php
@@ -113,7 +113,7 @@ if ($arResult['SHOW_ERRORS'] == 'Y' && $arResult['ERROR']) ShowMessage($arResult
 
         <?if($arResult["AUTH_SERVICES"]):?>
             <?
-            $APPLICATION->IncludeComponent("bitrix:socserv.auth.form", "popup",
+            $APPLICATION->IncludeComponent("bitrix:socserv.auth.form", "",
                 array(
                     "AUTH_SERVICES"=>$arResult["AUTH_SERVICES"],
                     "AUTH_URL"=>$arResult["AUTH_URL"],
